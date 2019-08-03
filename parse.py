6edc873d0baff9e7ce4a6413b78d0ffa6c0d94ee
Deev Pavel <deev@uchi.ru>
2019-08-03 15:54:33 +0300

    wait 60s

--- parse.py
+++ parse.py
@@ -41,7 +41,7 @@ def get_html(url, proxy, user_agent, need_element='*'):
     driver.set_page_load_timeout(10)
     driver.get(url)
     print(driver.find_element_by_tag_name('body').get_attribute('innerHTML'))
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(60)
     driver.find_element_by_class_name(need_element)
     html = driver.find_element_by_tag_name('body')
     html = html.get_attribute('innerHTML')
