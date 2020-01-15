5896347cf9d5a7c26e960311e04d14ef62ada7af
Deev Pavel <deev@uchi.ru>
2020-01-15 15:15:58 +0300

    add sh script

---
+++ cron.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for i in $(seq 1 5); do
+  node ~/ip_parser/dist/index.js > ~/ip_parser/log.txt
+#  /opt/bin/python3 /opt/etc/movies/digitalreleases2.py > /opt/etc/movies/log.txt
+  ret=$?
+  if [ $ret -eq 0 ]; then
+    logger -t "ip parser" "Загрузка завершена успешно."
+    break
+  else
+    logger -t "ip parser" "Ошибка загрузки."
+  fi
+done
